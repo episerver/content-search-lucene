a2dde0ffa2cc6fa7cebc22653234e04df6b0e178
Johan Björnfot <johan.bjornfot@episerver.com>
2014-02-25 16:19:33 +0100

    Added more descriptive exceptions if package locating fails

--- EPiServer.Search.IndexingService.Test/AssemblyInitializer.cs
+++ EPiServer.Search.IndexingService.Test/AssemblyInitializer.cs
@@ -16,10 +16,26 @@ namespace EPiServer.Search.IndexingService.Test
         public static void AssemblyInitialize(TestContext testContext)
         {
             var packagesFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\", "packages");
+
+            if (!Directory.Exists(packagesFolder))
+            {
+                throw new InvalidOperationException(String.Format("Cant set up database for CMS because nuget package folder was resolved to '{0}' which does not exist", packagesFolder));
+            }
+
             var packageRepository = new NuGet.LocalPackageRepository(packagesFolder);
 
             var cmsPackage = packageRepository.FindPackagesById("EPiServer.CMS.Core").FirstOrDefault();
+            if (cmsPackage == null)
+            {
+                throw new InvalidOperationException("Could not get EPiServer.CMS.Core package");
+            }
+
+
             var dbSchema = cmsPackage.GetFiles().Where(f => f.Path.EndsWith("EPiServer.Cms.Core.sql", StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+            if (cmsPackage == null)
+            {
+                throw new InvalidOperationException("Could not get \\tools\\EPiServer.Cms.Core.sql from EPiServer.CMS.Core package");
+            }
 
             var sqlFilePath = Path.Combine(packageRepository.Source, String.Format("{0}.{1}", cmsPackage.Id, cmsPackage.Version.ToString()), dbSchema.Path);
             var destinationFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sql");
