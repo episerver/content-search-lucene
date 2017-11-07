fe03b30b5c7399a1c8a2e9ee3d00253dea12dd93
Per Bjurström <per.bjurstrom@episerver.com>
2017-11-07 11:06:26 +0100

    EPiServer.Search.Cms could not be installed after EPiServer.Search
    Fixes CMS-8948

--- build/NugetPackaging/Tools/EPiServer.Search.Cms.Install.ps1
+++ build/NugetPackaging/Tools/EPiServer.Search.Cms.Install.ps1
@@ -4,3 +4,5 @@ import-Module (join-path $toolsPath "Search.psm1")
 
 Add-EPiBindingRedirect $installPath  $project
 
+Set-EPiBaseUri $project
+
--- build/NugetPackaging/Tools/EPiServer.Search.IndexingService.Install.ps1
+++ build/NugetPackaging/Tools/EPiServer.Search.IndexingService.Install.ps1
@@ -5,5 +5,3 @@ Write-Host 'Importing Search.psm1'
 import-Module (join-path $toolsPath "Search.psm1")
 
 Add-EPiBindingRedirect $installPath  $project
-
-Set-EPiBaseUri $project
