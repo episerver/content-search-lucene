8ee80347d5d08370b29dbb9f3d1c4867f91cab9a
Linh Hoang <linh.hoang@episerver.com>
2021-09-06 10:47:52 +0700

    Add GetSearchResultsXml, GetSearchResultsJson and GetNamedIndexes api
    Task: CMS-19919
    Story: CMS-19870

--- src/EPiServer.Search.IndexingService/Controllers/IndexingController.cs
+++ src/EPiServer.Search.IndexingService/Controllers/IndexingController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
@@ -50,21 +51,54 @@ namespace EPiServer.Search.IndexingService.Controllers
         [Route("search/?q={q}&namedindexes={namedindexes}&format=xml&offset={offset}&limit={limit}&accesskey={accesskey}")]
         public IActionResult GetSearchResultsXml(string q, string namedIndexes, string offset, string limit, string accessKey)
         {
-            return Ok();
+            if (!_securityHandler.IsAuthenticated(accessKey, AccessLevel.Read))
+            {
+                return StatusCode(401);
+            }
+
+            return Ok(GetSearchResults(q, namedIndexes, Int32.Parse(offset, CultureInfo.InvariantCulture), Int32.Parse(limit, CultureInfo.InvariantCulture)));
         }
 
         [HttpGet]
         [Route("search/?q={q}&namedindexes={namedindexes}&format=json&offset={offset}&limit={limit}&accesskey={accesskey}")]
         public IActionResult GetSearchResultsJson(string q, string namedIndexes, string offset, string limit, string accessKey)
         {
-            return Ok();
+            if (!_securityHandler.IsAuthenticated(accessKey, AccessLevel.Read))
+            {
+                return StatusCode(401);
+            }
+
+            return Ok(GetSearchResults(q, namedIndexes, Int32.Parse(offset, CultureInfo.InvariantCulture), Int32.Parse(limit, CultureInfo.InvariantCulture)));
         }
 
         [HttpGet]
         [Route("namedindexes/?accesskey={accesskey}")]
         public IActionResult GetNamedIndexes(string accessKey)
         {
-            return Ok();
+            if (!_securityHandler.IsAuthenticated(accessKey, AccessLevel.Read))
+            {
+                return StatusCode(401);
+            }
+
+            return Ok(IndexingServiceHandler.Instance.GetNamedIndexes());
         }
+
+        #region Private
+
+        private SyndicationFeedFormatter GetSearchResults(string q, string namedIndexes, int offset, int limit)
+        {
+            IndexingServiceSettings.IndexingServiceServiceLog.Debug(String.Format("Request for search with query parser with expression: {0} in named indexes: {1}", q, namedIndexes));
+
+            //Parse named indexes string from request
+            string[] namedIndexesArr = null;
+            if (!String.IsNullOrEmpty(namedIndexes))
+            {
+                char[] delimiter = { '|' };
+                namedIndexesArr = namedIndexes.Split(delimiter);
+            }
+
+            return IndexingServiceHandler.Instance.GetSearchResults(q, namedIndexesArr, offset, limit);
+        }
+        #endregion
     }
 }
