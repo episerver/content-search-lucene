c90f768941fa6bdd8d5b15d747341f7e3e6ebd61
Linh Hoang <linh.hoang@episerver.com>
2021-09-07 11:32:11 +0700

    Enable authentication for api
    Task: CMS-19929
    Story: CMS-19870

--- src/EPiServer.Search.IndexingService/Security/SecurityHandler.cs
+++ src/EPiServer.Search.IndexingService/Security/SecurityHandler.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.ServiceModel;
-//using EPiServer.Search.IndexingService.Configuration;
+using EPiServer.Search.IndexingService.Configuration;
 using Microsoft.AspNetCore.Http;
 
 namespace EPiServer.Search.IndexingService.Security
 {
     public class SecurityHandler
     {
-        private static IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public SecurityHandler(IHttpContextAccessor httpContextAccessor)
         {
@@ -17,42 +17,40 @@ namespace EPiServer.Search.IndexingService.Security
 
         protected internal virtual bool IsAuthenticated(string accessKey, AccessLevel accessLevel)
         {
-            // TO BE UPDATED
+            IndexingServiceSettings.IndexingServiceServiceLog.Debug(String.Format("Request for authorization for access key '{0}'", accessKey));
 
-            //IndexingServiceSettings.IndexingServiceServiceLog.Debug(String.Format("Request for authorization for access key '{0}'", accessKey));
+            //Always fail if no client access key is found in the request
+            if (String.IsNullOrEmpty(accessKey))
+            {
+                IndexingServiceSettings.IndexingServiceServiceLog.Error("No access key found. Access denied.");
+                return false;
+            }
 
-            ////Always fail if no client access key is found in the request
-            //if (String.IsNullOrEmpty(accessKey))
-            //{
-            //    IndexingServiceSettings.IndexingServiceServiceLog.Error("No access key found. Access denied.");
-            //    return false;
-            //}
+            //Check if the access key exists and get the client element
+            ClientElement elem;
+            if (!IndexingServiceSettings.ClientElements.TryGetValue(accessKey, out elem))
+            {
+                IndexingServiceSettings.IndexingServiceServiceLog.Error(String.Format("The access key: '{0}' was not found for configured clients. Access denied.", accessKey));
+                return false;
+            }
 
-            ////Check if the access key exists and get the client element
-            //ClientElement elem;
-            //if (!IndexingServiceSettings.ClientElements.TryGetValue(accessKey, out elem))
-            //{
-            //    IndexingServiceSettings.IndexingServiceServiceLog.Error(String.Format("The access key: '{0}' was not found for configured clients. Access denied.", accessKey));
-            //    return false;
-            //}
+            //Check level.
+            if (elem.ReadOnly && accessLevel == AccessLevel.Modify)
+            {
+                IndexingServiceSettings.IndexingServiceServiceLog.Error(String.Format("Modify request for access key '{0}' failed. Only read access", accessKey));
+                return false;
+            }
 
-            ////Check level.
-            //if (elem.ReadOnly && accessLevel == AccessLevel.Modify)
-            //{
-            //    IndexingServiceSettings.IndexingServiceServiceLog.Error(String.Format("Modify request for access key '{0}' failed. Only read access", accessKey));
-            //    return false;
-            //}
+            //Try to authenticate this request by configured client IP
+            var remoteIpAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress;
 
-            ////Try to authenticate this request by configured client IP
-            //var remoteIpAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress;
+            if (!elem.IsIPAddressAllowed(remoteIpAddress))
+            {
+                IndexingServiceSettings.IndexingServiceServiceLog.Error(string.Format("No match for client IP {0}. Access denied for access key {1}.", remoteIpAddress, accessKey));
+                return false;
+            }
 
-            //if (!elem.IsIPAddressAllowed(remoteIpAddress))
-            //{
-            //    IndexingServiceSettings.IndexingServiceServiceLog.Error(string.Format("No match for client IP {0}. Access denied for access key {1}.", remoteIpAddress, accessKey));
-            //    return false;
-            //}
-
-            //IndexingServiceSettings.IndexingServiceServiceLog.Debug(String.Format("Request for authorization for access key '{0}' succeded", accessKey));
+            IndexingServiceSettings.IndexingServiceServiceLog.Debug(String.Format("Request for authorization for access key '{0}' succeded", accessKey));
 
             return true;
         }
