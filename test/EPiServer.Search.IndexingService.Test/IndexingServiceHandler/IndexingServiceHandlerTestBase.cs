a06493f5a46cdac19cc0030250ca93f91684736b
Manh Hung Nguyen <manhhung.nguyen@episerver.com>
2021-09-10 17:04:45 +0700

    Add unit test for IndexingServiceHandler class
    Task: CMS-20018
    Story: CMS-19870

---
+++ test/EPiServer.Search.IndexingService.Test/IndexingServiceHandler/IndexingServiceHandlerTestBase.cs
@@ -0,0 +1,42 @@
+﻿using EPiServer.Logging.Compatibility;
+using EPiServer.Search.IndexingService.Helpers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPiServer.Search.IndexingService.Test.IndexingServiceHandler
+{
+    public class IndexingServiceHandlerTestBase
+    {
+        protected Mock<IFeedHelper> _feedHelperMock;
+        protected Mock<ILuceneHelper> _luceneHelperMock;
+        protected Mock<ICommonFunc> _commonFuncMock;
+        protected Mock<IResponseExceptionHelper> _responseExceptionHelperMock;
+        protected Mock<IDocumentHelper> _documentHelperMock;
+
+        public IndexingServiceHandlerTestBase()
+        {
+            _feedHelperMock = new Mock<IFeedHelper>();
+            _luceneHelperMock = new Mock<ILuceneHelper>();
+            _commonFuncMock = new Mock<ICommonFunc>();
+            _responseExceptionHelperMock = new Mock<IResponseExceptionHelper>();
+            _documentHelperMock = new Mock<IDocumentHelper>();
+
+            var logMock = new Mock<ILog>();
+            IndexingServiceSettings.IndexingServiceServiceLog = logMock.Object;
+        }
+
+        public EPiServer.Search.IndexingService.IndexingServiceHandler SetupMock()
+        {
+            return new IndexingService.IndexingServiceHandler(
+                _feedHelperMock.Object,
+                _luceneHelperMock.Object,
+                _commonFuncMock.Object,
+                _responseExceptionHelperMock.Object,
+                _documentHelperMock.Object);
+        }
+    }
+}
---
+++ test/EPiServer.Search.IndexingService.Test/IndexingServiceHandler/ResetNamedIndexTest.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EPiServer.Search.IndexingService.Test.IndexingServiceHandler
+{
+    [Trait(nameof(EPiServer.Search.IndexingService.IndexingServiceHandler), nameof(EPiServer.Search.IndexingService.IndexingServiceHandler.ResetNamedIndex))]
+    public class ResetNamedIndexTest : IndexingServiceHandlerTestBase
+    {
+        [Fact]
+        public void ResetNamedIndex_WhenNamedIndexNotExist_ShouldThrowExeption()
+        {
+            var namedIndexMock1 = new Mock<NamedIndex>(Guid.NewGuid().ToString());
+            var namedIndexMock2 = new Mock<NamedIndex>(Guid.NewGuid().ToString());
+            IndexingServiceSettings.NamedIndexElements.Add(namedIndexMock1.Object.Name, new Configuration.NamedIndexElement() { Name = namedIndexMock1.Object.Name });
+
+            _responseExceptionHelperMock.Setup(x => x.HandleServiceError(It.IsAny<string>())).Throws(new HttpResponseException() { Status = 500 });
+
+            var classInstant = SetupMock();
+            var caughtExeption = Assert.Throws<HttpResponseException>(() => classInstant.ResetNamedIndex(namedIndexMock2.Object.Name));
+
+            Assert.Equal(500, caughtExeption.Status);
+        }
+
+        [Fact]
+        public void ResetNamedIndex_WhenNamedIndexExist_ShouldWork()
+        {
+            var namedIndexMock1 = new Mock<NamedIndex>(Guid.NewGuid().ToString());
+            IndexingServiceSettings.NamedIndexElements.Add(namedIndexMock1.Object.Name, new Configuration.NamedIndexElement() { Name = namedIndexMock1.Object.Name });
+
+            var classInstant = SetupMock();
+            classInstant.ResetNamedIndex(namedIndexMock1.Object.Name);
+        }
+    }
+}
